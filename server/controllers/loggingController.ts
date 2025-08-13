a2062791ff9be0bfe652b0c06da576783b52b28c
Lisa Louison <lisa.louison@gmail.com>
2025-08-13 16:38:07 -0500

    feature(logging): create log controller

--- server/controllers/loggingController.ts
+++ server/controllers/loggingController.ts
@@ -1,3 +1,25 @@
 import { RequestHandler } from 'express';
 import { ServerError } from '../../types/types';
-import dotenv from 'dotenv';
+import Log from '../models/logModel';
+
+export const logQuery: RequestHandler = async (_req, res, next) => {
+  const { userSign, userQuery, allQuoteRecommendations, embedding } =
+    res.locals;
+
+  try {
+    await Log.insertOne({
+      userSign,
+      userQuery,
+      allQuoteRecommendations,
+      embedding,
+    });
+    return next();
+  } catch (err) {
+    const error: ServerError = {
+      log: `logQuery: ${err}`,
+      status: 500,
+      message: { err: `An error occured while logging query to mongodb` },
+    };
+    return next(error);
+  }
+};
