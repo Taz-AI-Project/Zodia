7d87c6781f7f2fc269bf526cfbfd8ef8ebb557cf
yuan-cloud <eucapop@gmail.com>
2025-08-13 22:26:11 -0400

    Update express.d.ts

--- types/express.d.ts
+++ types/express.d.ts
@@ -1,6 +1,7 @@
 declare namespace Express {
   export interface Locals {
     userQuery?: string;
+    zodiac?: string;
     embedding?: number[];
     pineconeQueryResult?: import('@pinecone-database/pinecone').ScoredPineconeRecord<
       import('./types').QuoteMetadata
