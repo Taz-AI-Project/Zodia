eaf938e2e62227abe44d993a8a9a4d0578e1c862
Michael Zhao <muqingzhao526@gmail.com>
2025-08-14 08:15:40 -0700

    added pinconeController logic

--- server/controllers/pineconeController.ts
+++ server/controllers/pineconeController.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express';
-import { ServerError, MovieMetadata } from '../../types/types';
+import { ServerError, QuoteMetadata } from '../../types/types';
 import { Pinecone } from '@pinecone-database/pinecone';
 import dotenv from 'dotenv';
 
@@ -14,65 +14,65 @@ export const queryPineconeDatabase: RequestHandler = async (
   res,
   next
 ) => {
-//   const { embedding } = res.locals;
-//   if (!embedding) {
-//     const error: ServerError = {
-//       log: 'Database query middleware did not receive embedding',
-//       status: 500,
-//       message: { err: 'An error occurred before querying the database' },
-//     };
-//     return next(error);
-//   }
-//   try{
-//     const index = pc.index<MovieMetadata>(process.env.PINECONE_INDEX!);
-//     const result = await index.query({
-//       vector: embedding,
-//       topK: 3,
-//       includeMetadata: true,
-//     })
-//     console.log(result.matches);
-//     res.locals.pineconeQueryResult = result.matches;
-//     return next();
-//   }catch(err){
-//     const error = {
-//       log: `queryPineconeDatabase: ${err}`,
-//       status: 500,
-//       message: { err: 'An error occurred while querying database' },
-//     }
-//     return next(error);
-//   }
-    res.locals.pineconeQueryResult = [
-      {
-        id: '1',
-        score: 0.54014945,
-        values: [],
-        sparseValues: undefined,
-        metadata: {
-            id: '1',
-            content: 'You are amazing!',
-        }
-        },
-        {
-        id: '2',
-        score: 0.54014945,
-        values: [],
-        sparseValues: undefined,
-        metadata: {
-            id: '2',
-            content: 'I am amazing!',
-        }
-        },
-        {
-        id: '3',
-        score: 0.54014945,
-        values: [],
-        sparseValues: undefined,
-        metadata: {
-            id: '3',
-            content: 'They are amazing!',
-        }
-        },
-      
-    ];
+  const { embedding } = res.locals;
+  if (!embedding) {
+    const error: ServerError = {
+      log: 'Database query middleware did not receive embedding',
+      status: 500,
+      message: { err: 'An error occurred before querying the database' },
+    };
+    return next(error);
+  }
+  try{
+    const index = pc.index<QuoteMetadata>(process.env.PINECONE_INDEX!);
+    const result = await index.query({
+      vector: embedding,
+      topK: 3,
+      includeMetadata: true,
+    })
+    console.log(result.matches);
+    res.locals.pineconeQueryResult = result.matches;
     return next();
+  }catch(err){
+    const error = {
+      log: `queryPineconeDatabase: ${err}`,
+      status: 500,
+      message: { err: 'An error occurred while querying database' },
+    }
+    return next(error);
+  }
+    // res.locals.pineconeQueryResult = [
+    //   {
+    //     id: '1',
+    //     score: 0.54014945,
+    //     values: [],
+    //     sparseValues: undefined,
+    //     metadata: {
+    //         id: '1',
+    //         content: 'You are amazing!',
+    //     }
+    //     },
+    //     {
+    //     id: '2',
+    //     score: 0.54014945,
+    //     values: [],
+    //     sparseValues: undefined,
+    //     metadata: {
+    //         id: '2',
+    //         content: 'I am amazing!',
+    //     }
+    //     },
+    //     {
+    //     id: '3',
+    //     score: 0.54014945,
+    //     values: [],
+    //     sparseValues: undefined,
+    //     metadata: {
+    //         id: '3',
+    //         content: 'They are amazing!',
+    //     }
+    //     },
+      
+    // ];
+    // return next();
 };
