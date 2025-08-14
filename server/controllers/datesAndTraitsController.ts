650dc6d81601de2c7a23817300822b8af773d8b1
Michael Zhao <muqingzhao526@gmail.com>
2025-08-14 14:59:34 -0700

    added zodiac dates and traits to mongo

---
+++ server/controllers/datesAndTraitsController.ts
@@ -0,0 +1,7 @@
+import { RequestHandler } from 'express';
+import { ServerError } from '../../types/types';
+import { ZodiacInfo } from '../models/zodiacModel';
+
+export const zodiacQuery: RequestHandler = async (_req, res, next) => {
+
+}
