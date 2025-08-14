43938407be629143d43071101eb222a9a39306cf
harleydi <disneyharley26@gmail.com>
2025-08-13 21:13:15 -0400

    feat(frontend): created basic MVP structure, started sumbit handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { cards } from './mockData'
+import Card from './Card'
 
 function App() {
-  return <h1>Zodiac project</h1>
+  const [zodiacSign, setZodiacSign] = useState('')
+  const [feelingContent, setFeelingContent] = useState('')
+  const [cardsInfo, setCardsInfo] = useState<string[]>([])
+
+  useEffect(() => {
+    setCardsInfo([...cards])
+  }, [])
+
+  const onSubmitHandler = async () => {
+    try {
+      const response = fetch('/api', {
+        method: 'POST',
+        headers: { 'ContentType': 'application/json' },
+        body: JSON.stringify({ zodiacSign, feelingContent })
+      })
+    } catch (error) {
+      console.error('onSubmitHandler Error: ', error)
+    }
+  }
+
+  return (
+    <div>
+      <h1 className='text-red-500'>Zodiac project</h1>
+      <div>
+        <p>Whats your sign?</p>
+        <input type="text" name="" id="" onChange={(e) => setZodiacSign(e.target.value)} />
+      </div>
+      <div>
+        <p>How are you feeling?</p>
+        <input type="text" name="" id="" onChange={(e) => setFeelingContent(e.target.value)} />
+      </div>
+      <button type="submit">Submit</button>
+      <div id="cards" className='flex justify-center'>
+        {
+          cardsInfo.map((card, index) => <Card cardInfo={card} index={index} />)
+        }
+      </div>
+    </div>
+  )
 }
 
 export default App
