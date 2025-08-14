70eba6285cd465a6daf754f49dd6fdefd4bc32b9
Lisa Louison <lisa.louison@gmail.com>
2025-08-13 19:08:28 -0500

    refactor(mock): add mock data for log testing

--- server/controllers/loggingController.ts
+++ server/controllers/loggingController.ts
@@ -1,18 +1,28 @@
 import { RequestHandler } from 'express';
 import { ServerError } from '../../types/types';
-import Log from '../models/logModel';
+import Log from '../models/logModel.js';
 
 export const logQuery: RequestHandler = async (_req, res, next) => {
-  const { userSign, userQuery, allQuoteRecommendations, embedding } =
-    res.locals;
+  console.log('in log controller');
+  //   const { userSign, userQuery, allQuoteRecommendations, embedding, finalQuote } =
+  //     res.locals;
+
+  //TO DO: test data to be removed
+  const userSign = 'Aquarius';
+  const userQuery = 'I feel happy about coding';
+  const allQuoteRecommendations = ['quote 1', 'quote 2', 'quote 3'];
+  const embedding = [0, 1, 2];
+  const finalQuote = 'You are amazing!';
 
   try {
-    await Log.insertOne({
+    const log = await Log.insertOne({
       userSign,
       userQuery,
       allQuoteRecommendations,
+      finalQuote,
       embedding,
     });
+    res.locals.log = log;
     return next();
   } catch (err) {
     const error: ServerError = {
